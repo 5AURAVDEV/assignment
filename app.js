c846eed06710cb18c1fb39aca3d453eb1d6b7482
Saurav Dev <devofficial998@gmail.com>
2023-07-03 18:07:01 +0530

    new commits

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-let x=document.getElementById("header-title")
- console.log(x.textContent)
+//let x=document.getElementById("header-title")
+//console.log(x.textContent)
+let y=document.getElementsByClassName("list-group-item")
+y[2].style.backgroundColor="green"
+for(let i=0;i<y.length;i++){
+    y[i].style.fontWeight="bold"
+}
